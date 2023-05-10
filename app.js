349f8bc41696b8cc2088ebc38ce8875fcebb6dc4
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-05-10 20:07:29 +0000

    Added Cat Fact API and resources

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Establish Requirements
+const { toFormData } = require('axios');
 var express = require('express');
 var app = express();
 var axios = require('axios').default;
@@ -6,16 +7,26 @@ var axios = require('axios').default;
 app.use('/static', express.static("public"));
 app.set("view engine", "ejs");
 
-// Create "Random Cat Generator" Method
+// Default Page - Random Cat Generator
 app.get('/', function(req, res){
-    // Create array
+    // Generate Random Image
+
+    // Initialize variables
     let catData = {}
 
-    // Get API data AND Set data in array
+    // Call API and retrieve cat image
     axios.get('https://api.thecatapi.com/v1/images/search').then(function(response){
         catData = response.data[0]
-        res.render('todo.ejs', {catData: catData});
     })
+
+    // Initialize variables
+    let catFact = {}
+
+    axios.get('https://catfact.ninja/fact').then(function(response){
+        catFact = response.data
+        res.render('todo.ejs', {catData: catData, catFact: catFact})
+    })
+
 })
 
 // Identify Port (3000 was unavailable)
